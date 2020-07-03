1c743fe9302928af7cc5561b65e2880e5bc9fe5c
Red Fairy <agorelik2@gmail.com>
2020-07-03 17:13:33 -0400

    added js functions to show right and wrong answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,9 +48,39 @@ function showQuestion(question){
 }
 
   function selectAnswer(e) {
-
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    //Create an array of all answers and set the data element correct to 'correct' or 'wrong'
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+  })
+    if (jsQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+  } 
+  else {
+        startButton.innerText = 'Try again'
+        startButton.classList.remove('hide')
+    }
 
   }
+
+  // Set display class of the answer element based on 'correct' class
+  function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+
+  // Clear the display class for the answers
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
+
   const questions = [
     {
       question: 'Inside which HTML element do we put the JavaScript?',
