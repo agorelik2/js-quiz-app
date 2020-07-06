139c0063e50e1e8a78d83b6d4bbe9cf16e5c8025
Red Fairy <agorelik2@gmail.com>
2020-07-05 21:35:22 -0400

    Changed code to show the red/green button for user to see correct or wrong answer. Created the display of results at the end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,13 @@
+const quizContainerElement = document.getElementById('quiz-container')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
+const quizStatus = document.getElementById('quiz-status')
+const userInit = document.getElementById ('user-input')
 
-let jsQuestions, currentQuestionIndex
-
-startButton.addEventListener('click', startQuiz)
-nextButton.addEventListener('click', () => {
-    currentQuestionIndex++
-    displayNextQuestion()
-  })
-
-function startQuiz() {
-    startButton.classList.add('hide')
-    jsQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide')
-    displayNextQuestion()
-  }
-
-function displayNextQuestion() {
-    resetPage()
-    showQuestion(jsQuestions[currentQuestionIndex])
-  }
-
-function resetPage(){
-
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
-}
-function showQuestion(question){
-
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-          button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-      })
-}
-
-  function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    //Create an array of all answers and set the data element correct to 'correct' or 'wrong'
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-  })
-    if (jsQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove('hide')
-  } 
-  else {
-        startButton.innerText = 'Try again'
-        startButton.classList.remove('hide')
-    }
-
-  }
-
-  // Set display class of the answer element based on 'correct' class
-  function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-      element.classList.add('correct')
-    } else {
-      element.classList.add('wrong')
-    }
-  }
-
-  // Clear the display class for the answers
-  function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-  }
-
-  const questions = [
+const questions = [
     {
       question: 'Inside which HTML element do we put the JavaScript?',
       answers: [
@@ -119,3 +45,123 @@ function showQuestion(question){
       ]
     }
   ]
+
+let jsQuestions, currentQuestionIndex, score
+
+quizStatus.innerText = "Coding Quiz Challenge"
+quizStatus.classList.remove('hide')
+userInit.classList.add('hide')
+startButton.addEventListener('click', startQuiz)
+console.log ("number of questions: " + questions.length)
+nextButton.addEventListener('click', () => 
+{
+    console.log ("before question index: " + currentQuestionIndex)
+    currentQuestionIndex++
+    console.log ("curent question index: " + currentQuestionIndex)
+    console.log ("number of questions: " + questions.length)
+    if (questions.length >= currentQuestionIndex + 1){
+        displayNextQuestion()
+    } else {
+       console.log ("you are done") 
+       displayResults()
+    }
+  })
+
+function startQuiz() {
+    startButton.classList.add('hide')
+    quizStatus.classList.add('hide')
+    userInit.classList.add('hide')
+    jsQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    score = 0
+    questionContainerElement.classList.remove('hide')
+    displayNextQuestion()
+  }
+
+function displayNextQuestion() {
+    resetPage()
+    showQuestion(jsQuestions[currentQuestionIndex])
+  }
+
+function resetPage(){
+
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+function showQuestion(question){
+
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        //if (answer.correct) {
+          button.dataset.correct = answer.correct
+        
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+      })
+}
+
+  function selectAnswer(e) {
+    const selectedButton = e.target
+    let correct = selectedButton.dataset.correct
+    let selected = 0
+    
+    //Create an array of all answers and set the data element correct to 'correct' or 'wrong'
+    //setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        console.log ("inside buttons array")
+        if (button == selectedButton) {          
+            console.log ("button selected")
+            selected = 1  
+        } else {
+            selected = 0
+        }
+        setStatusClass(selected, button, button.dataset.correct)
+        button.disabled = true
+  })
+  
+    if (jsQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')   
+    } 
+    else {
+       nextButton.classList.remove('hide')
+       nextButton.innerText = 'Finish'
+    //   nextButton.addEventListener('click', displayResults)
+      console.log ("after display results")     
+    }
+}
+ 
+  // Set display class of the answer element based on 'correct' class
+  function setStatusClass(clicked, element, correct) {
+    clearStatusClass(element)
+ 
+   if (clicked == 1 && correct == 'true') {
+        element.classList.add('correct')
+        score = score + 2
+    } else if (clicked == 1 && correct == 'false'){
+            element.classList.add('wrong')
+            console.log ("clicked wrong button")
+    } else { console.log ("did not click this button")}
+  }
+
+  // Clear the display class for the answers
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
+
+  function displayResults() {
+    questionContainerElement.classList.add('hide')
+    nextButton.classList.add('hide')
+    quizStatus.innerText = "All Done! Your score is " + score
+    startButton.innerText = 'Try again'
+    nextButton.innerText = 'Next'
+    userInit.classList.remove('hide')
+    startButton.classList.remove('hide')
+    quizStatus.classList.remove('hide')
+    currentQuestionIndex = 0
+}
